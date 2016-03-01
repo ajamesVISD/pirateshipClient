92f5c5dbca6f331f9ae89f88dedec074af8730f5
Andy James <ajames@vashonsd.org>
2016-03-01 08:15:55 -0800

    First setup

---
+++ pirateshipClient/src/main/java/org/vashonsd/pirateshipClient/App.java
@@ -0,0 +1,13 @@
+package org.vashonsd.pirateshipClient;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
